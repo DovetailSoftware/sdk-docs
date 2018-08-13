7ea59493864c4058cda10cba7fdc9eabb6184bda
Sam Tyson <styson@gmail.com>
2018-08-13 14:56:54 -0500

    update for automated help install and chm build

--- build-chm.bat
+++ build-chm.bat
@@ -1,4 +1,8 @@
 @echo off
+del hhw\*.* /Q
+htmlhelp.exe /Q /T:%cd%\hhw /C
+regsvr32 %cd%\hhw\itcc.dll /s
+
 rem make a backup copy of _site js/css
 copy docfx_project\_site\styles\main.css override\hold-main.css
 copy docfx_project\_site\styles\docfx.js override\hold-docfx.js
